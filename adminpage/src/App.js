fc3d53a1e90c10c452f5e3709b37fb5436086b54
nidhiedigaa <www.nidhinnediga@gmail.com>
2023-11-29 19:18:26 +0530

    steps to implement chart

--- adminpage/src/App.js
+++ adminpage/src/App.js
@@ -3,18 +3,12 @@ import './App.css';
 import Invoices from './components/pageone/invoices';
 import Clients from './components/pageone/clients';
 import Payments from './components/pageone/payments';
-import LineChart from './components/chart/check';
+import ChartOne from "./components/chart/check"
 import RecentProjects from './components/pageone/recentprojects';
 function App() {
   return (
     <div className="App">
-      <div className="admin-page-3 ">
-      <Invoices></Invoices>
-      <Payments></Payments>
-      <Clients></Clients>
-      <RecentProjects></RecentProjects>
-      <LineChart></LineChart>
-      </div>
+      <ChartOne></ChartOne>
     </div>
   );
 }
--- adminpage/src/components/chart/check.js
+++ adminpage/src/components/chart/check.js
@@ -1,41 +1,40 @@
-import {Line} from "react-chartjs-2"
 import React from "react"
+import Chart from "chart.js/auto"
+import {Line} from "react-chartjs-2"
 
-function LineChart(props)
+
+
+function ChartOne(props)
 {
-
     const data={
-        labels:['jan','feb','mar','apr','may'],
+        labels:['JAN','FEB','MAR','APR','MAY'],
         datasets:[
             {
-                label:'sales for 2020 in (M)',
-                data:[3,2,2,1,5]
+                label:"sales for 2020 (M)",
+                data:[3,2,1,3,5],
+                backgroundColor: 'rgba(75,192,192,0.2)', // Background color for the bars
+                borderColor: 'rgba(75,192,192,1)', // Border color for the bars
+                borderWidth: 1,
+            },
+            {
+                label:"sales for 2020 (M)",
+                data:[2,3,4,5,2]
             }
         ]
     }
-    const options={
-        title:{
-            display:true,
-            text:'Line Chart'
-        },
-        scales:{
-            yAxes:[
-                {
-                    ticks:
-                    {
-                        min:0,
-                        max:6,
-                        stepSize:1
-                    }
-                }
-            ]
-        }
-    }
-    return(<>
-    <div>
-        <Line data={data}></Line>
-    </div>
+
+
+
+
+
+
+
+
+
+    return (<>
+    <Line data={data}></Line>
     </>)
 }
 
-export default LineChart
+
+export default ChartOne
